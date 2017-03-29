d709ecdd627132ca2eb9c6d8fdbbf7e9e9bb94de
Adrian Besleaga <ady.shake@yahoo.com>
2017-03-29 22:55:48 +0300

    Update problema 1

--- Problema_1.java
+++ Problema_1.java
@@ -7,47 +7,58 @@ public class Problema {
 
 	public static void main(String[] args) {
 		int input[] = input();
-		int x = input[0];
-		int y = input[1];
-		if(x+y < 38){
-			matrice(x,y);
+		int n = input[0];
+		int k = input[1];
+		if(n<37){
+			matrice(n,k);
 		}else{
-			System.out.println("Valorile sunt prea mari");
+			System.out.println("n trebuie sa fie mai mic decat 37");
 		}
+		
 
 	}
 
 	public static int[] input(){
-		int x=0; //initializare variabile
-		int y=0;
+		int n=0; 
+		int k=0;
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("Valoarea lui x: ");
+		System.out.print("Valoarea lui n: ");
 		try{
-			x = scanner.nextInt();
-			System.out.print("Valoarea lui y: ");
-			y = scanner.nextInt();
+			n = scanner.nextInt();
+			System.out.print("Valoarea lui k: ");
+			k = scanner.nextInt();
 		}catch(InputMismatchException exception){ // validare int
 			System.out.println("Doar numere int");
 		}
-		scanner.close(); // Aici am rezolvat "Resource leak: 'scanner' is never closed" 
-		return new int[] {x, y};
+		scanner.close(); 
+		return new int[] {n,k};
 
 	}
 
-	public static void matrice(int x,int y){
-		String[][] matrice = new String[x][y];
+	public static void matrice(int n,int k){
 		String data[] = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"};
-		
-		for(int i=0;i<x;i++){
-			for(int j=0;j<y;j++){
-				matrice[i][j]=data[i+j];
-				System.out.print(matrice[i][j] + " ");				
+		for(int z=0;z<k;z++){
+			int x = z * n ; 
+			for(int i=0;i<n;i++){
+				for(int j=0;j<n;j++){
+					if(i+j+x < data.length){
+					System.out.print(data[i+j+x] + " ");
+					}else{
+						if(j==0 && i+j+x-data.length == 0){
+							//verificare ultima linie posibila fara repetare
+							z=k;
+							i=n;
+							j=n;
+							System.out.println("............");
+						}else{
+							System.out.print(data[i+j+x-data.length] + " ");
+						}
+					}
+				}
+				System.out.println();								
 			}
-			System.out.println();					
-			
+			System.out.println();
 		}
-		
-
 	}
 
 }
